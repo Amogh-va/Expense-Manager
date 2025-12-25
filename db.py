d1bc14340aceadeee42f2782835fd8e4aea3d6d0
Amogh-va <amogharunp@gmail.com>
2025-12-24 21:04:53 -0500

    db.py
    Redacted sensitive database credentials in get_db_connection function.

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ import mysql.connector
 def get_db_connection():
     connection = mysql.connector.connect(
         host = 'localhost',
-        username = 'root',
-        password = 'Tiger@123',
+        username = REDACTED,
+        password = REDACTED,
         database = 'expense_manager'
     )
 
